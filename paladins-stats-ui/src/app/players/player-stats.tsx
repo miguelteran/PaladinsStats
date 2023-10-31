d2644a63beb2744554b1fe2a0426669fb6853f01
miguelteran <miguel.teranbenalcazar@alum.utoronto.ca>
2023-10-30 22:46:12 -0500

    Couple improvements

--- paladins-stats-ui/src/app/players/player-stats.tsx
+++ paladins-stats-ui/src/app/players/player-stats.tsx
@@ -2,36 +2,36 @@
 
 import { Key, useState, useCallback } from 'react';
 import { RecentMatch } from '@miguelteran/paladins-api-wrapper';
-import { CustomTable, CustomTableColumn } from '@/components/table';
+import { CustomTable, CustomTableColumn, CustomTableSortingDirection } from '@/components/table';
 import { Tab, Tabs } from '@nextui-org/tabs';
 import { SortDescriptor } from '@nextui-org/react';
 import { getPercentageString, getTimeString } from '@/util/string-util';
 import { ChampionStatsSummary } from '@/models/champion-stats-summary';
 
 
+const matchesTableColumns: CustomTableColumn[] = [
+    { key: 'Champion', label: 'Champion' },
+    { key: 'Map_Game', label: 'Mode' },
+    { key: 'Win_Status', label: 'Result' },
+    { key: 'KDA', label: 'KDA' },
+    { key: 'Match_Time', label: 'Date' }
+];
+
+const championsTableColumns: CustomTableColumn[] = [
+    { key: 'championName', label: 'Champion', sortable: true },
+    { key: 'rank', label: 'Level', sortable: true },
+    { key: 'numberOfMatches', label: 'Total Matches', sortable: true },
+    { key: 'winRate', label: 'Win Rate', sortable: true },
+    { key: 'kdaRatio', label: 'KDA', sortable: true },
+    { key: 'minutesPlayed', label: 'Time Played', sortable: true }
+]
+
 export const PlayerStats = ({recentMatches, championStats}: {recentMatches: RecentMatch[], championStats: ChampionStatsSummary[]}) => {
 
-    const [championsSortDescriptor, setChampionsSortDescriptor] = useState<SortDescriptor>({
+    const [ championsSortDescriptor, setChampionsSortDescriptor ] = useState<SortDescriptor>({
         column: 'champion',
-        direction: 'ascending',
+        direction: CustomTableSortingDirection.ASCENDING_SORTING_DIRECTION,
     });
-
-    const matchesTableColumns: CustomTableColumn[] = [
-        {key: 'Champion', label: 'Champion'},
-        {key: 'Map_Game', label: 'Mode'},
-        {key: 'Win_Status', label: 'Result'},
-        {key: 'KDA', label: 'KDA'},
-        {key: 'Match_Time', label: 'Date'}
-    ];
-
-    const championsTableColumns: CustomTableColumn[] = [
-        {key: 'championName', label: 'Champion', sortable: true},
-        {key: 'rank', label: 'Level', sortable: true},
-        {key: 'numberOfMatches', label: 'Total Matches', sortable: true},
-        {key: 'winRate', label: 'Win Rate', sortable: true},
-        {key: 'kdaRatio', label: 'KDA', sortable: true},
-        {key: 'minutesPlayed', label: 'Time Played', sortable: true}
-    ]
 
     const renderMatchesCell = useCallback((match: RecentMatch, columnKey: Key) => {
         if (columnKey === 'KDA') {
--- paladins-stats-ui/src/components/table.tsx
+++ paladins-stats-ui/src/components/table.tsx
@@ -1,17 +1,31 @@
 'use client'
 
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from '@nextui-org/table';
-import { SortDescriptor } from '@nextui-org/react';
+import { Spinner, Pagination, SortDescriptor } from '@nextui-org/react';
 import { Key, useCallback, useMemo } from 'react';
 
 
+export enum CustomTableSortingDirection {
+    ASCENDING_SORTING_DIRECTION = 'ascending',
+    DESCENDING_SORTING_DIRECTION = 'descending'
+}
+
+export enum CustomTableLoadingState {
+    LOADING = 'loading',
+    IDLE = 'idle'
+}
+
+type LoadingState = 'loading' | 'sorting' | 'loadingMore' | 'error' | 'idle' | 'filtering'
+
 export interface CustomTableProps<T> {
-    columns: CustomTableColumn[],
+    columns?: CustomTableColumn[],
     rows: T[],
     tableRowKey: string,
     customCellRenderer?: (item: T, columnKey: Key) => any,
     sortDescriptor?: SortDescriptor,
     onSortChange?: (sortDescriptor: SortDescriptor) => void,
+    loadingState?: LoadingState,
+    paginationParams?: CustomTablePaginationParams
 }
 
 export interface CustomTableColumn {
@@ -20,44 +34,81 @@ export interface CustomTableColumn {
     sortable?: boolean
 }
 
+export interface CustomTablePaginationParams {
+    numPages: number;
+    activePage: number;
+    onPageChange: (activePage: number) => void;
+}
+
 export function CustomTable<T>(props: CustomTableProps<T>) {
 
+    const { columns, rows, tableRowKey, sortDescriptor, paginationParams, loadingState, customCellRenderer, onSortChange } = props;
+
     const sortedRows = useMemo(() => {
-        if (!props.sortDescriptor) {
-            return props.rows;
+        if (!sortDescriptor) {
+            return rows;
         }
-        return [...props.rows].sort((a: T, b: T) => {
-            const first = a[props.sortDescriptor!.column as keyof T] as number;
-            const second = b[props.sortDescriptor!.column as keyof T] as number;
+        return rows.sort((a: T, b: T) => {
+            const first = a[sortDescriptor.column as keyof T] as number;
+            const second = b[sortDescriptor.column as keyof T] as number;
             const cmp = first < second ? -1 : first > second ? 1 : 0;
-            return props.sortDescriptor!.direction === 'descending' ? -cmp : cmp;
+            return sortDescriptor.direction === CustomTableSortingDirection.DESCENDING_SORTING_DIRECTION ? -cmp : cmp;
         });
-    }, [props.sortDescriptor, props.rows]);
+    }, [sortDescriptor, rows]);
 
     const renderCell = useCallback((item: T, columnKey: Key) => {
         let cellContent = undefined;
-        if (props.customCellRenderer) {
-            cellContent = props.customCellRenderer(item, columnKey);
+        if (customCellRenderer) {
+            cellContent = customCellRenderer(item, columnKey);
         } 
         return cellContent !== undefined ? cellContent : getKeyValue(item, columnKey);
     }, []);
 
+    const getPagination = () => {
+        if (!paginationParams) {
+            return undefined;
+        }
+        const { activePage, numPages, onPageChange } = paginationParams;
+        if (numPages > 0) {
+            return (
+                <div className='flex w-full justify-center'>
+                    <Pagination
+                        isCompact
+                        showControls
+                        showShadow
+                        color='secondary'
+                        page={activePage}
+                        total={numPages}
+                        onChange={onPageChange}
+                    />
+                </div>
+            );
+        } else {
+            return undefined;
+        }
+    }
+
     return (
         <Table
             isHeaderSticky
-            sortDescriptor={props.sortDescriptor}
-            onSortChange={props.onSortChange}
+            sortDescriptor={sortDescriptor}
+            onSortChange={onSortChange}
+            bottomContent={getPagination()}
         >
-            <TableHeader columns={props.columns}>
+            <TableHeader columns={columns}>
                 {(column) => 
                     <TableColumn key={column.key} allowsSorting={column.sortable}>
                         {column.label}
                     </TableColumn>
                 }
             </TableHeader>
-            <TableBody<T> items={sortedRows}>
+            <TableBody<T> 
+                items={sortedRows}
+                loadingContent={<Spinner/>}
+                loadingState={loadingState}
+            >
                 {(item) => (
-                    <TableRow key={getKeyValue(item, props.tableRowKey)}>
+                    <TableRow key={getKeyValue(item, tableRowKey)}>
                         {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
                     </TableRow>
                 )}
