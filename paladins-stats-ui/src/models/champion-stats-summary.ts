4ddb5fe71b86efc6ed2684f3802fc3dc649341a1
miguelteran <miguel.teranbenalcazar@alum.utoronto.ca>
2023-12-05 22:32:29 -0500

    Adding role

--- paladins-stats-ui/src/models/champion-stats-summary.ts
+++ paladins-stats-ui/src/models/champion-stats-summary.ts
@@ -1,6 +1,9 @@
+import { PaladinsRoles } from "./role";
+
 export interface ChampionStatsSummary {
     championId: string;
     championName: string;
+    championRole: PaladinsRoles
     rank: number;
     numberOfMatches: number;
     winRate: number;
