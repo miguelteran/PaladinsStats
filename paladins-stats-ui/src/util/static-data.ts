37015605bfcfabd7e63ec4571ca91057355a5b06
miguelteran <miguel.teranbenalcazar@alum.utoronto.ca>
2024-02-08 21:51:35 -0500

    Forgot to commit function

--- paladins-stats-ui/src/util/static-data.ts
+++ paladins-stats-ui/src/util/static-data.ts
@@ -1,7 +1,12 @@
 import championsList from '../../public/champions.json';
+import ranks from '../../public/ranks.json';
 
 export const championsMap = toMap(championsList, 'id');
 
 function toMap(entries: any[], key: string) {
     return new Map(entries.map(e => [e[key], e]))
+}
+
+export function getRank(rankId: number) {
+    return ranks.find(rank => rank.id === rankId)?.name;
 }
