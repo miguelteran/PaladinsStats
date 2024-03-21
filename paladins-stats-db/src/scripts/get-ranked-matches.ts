b3a91261f98d81fab61f8ca3564088254edbb123
miguelteran <miguel.teranbenalcazar@alum.utoronto.ca>
2024-03-20 21:31:51 -0500

    Adding exception handling in get-ranked-matches script

--- paladins-stats-db/src/scripts/get-ranked-matches.ts
+++ paladins-stats-db/src/scripts/get-ranked-matches.ts
@@ -82,82 +82,105 @@ async function getRankedMatches(paladinsStatsDB: PaladinsStatsDatabase, date: st
     const championMatchesDAL = paladinsStatsDB.getDal(PaladinsStatsCollections.CHAMPION_MATCHES) as ChampionMatchesDAL;
     const championBansDAL = paladinsStatsDB.getDal(PaladinsStatsCollections.CHAMPION_BANS) as ChampionBansDAL;
 
-    const matchSummaries = await getMatchIdsByQueue(RANKED_QUEUE, date, time);
+    let matchSummaries;
+    
+    try {
+        matchSummaries = await getMatchIdsByQueue(RANKED_QUEUE, date, time);
+    } catch(e) {
+        console.log('An error occurred while getting match ids', e);
+        return;
+    }
+    
     const batches = Math.ceil(matchSummaries.length / BATCH_SIZE);
     
     for (let i=0; i<batches; i++) {
+        
         const start = i * BATCH_SIZE;
         const end = start + BATCH_SIZE;
         const matchIds: string[] = matchSummaries.slice(start, end).map(match => match.Match);
-        const matchDetailsList = await getMatchDetailsBatch(matchIds);
+
+        let matchDetailsList;
+        
+        try {
+            matchDetailsList = await getMatchDetailsBatch(matchIds);
+        } catch(e) {
+            console.log('An error occurred while getting match details', e);
+            continue;
+        }
 
         // Go through each match
 
         for (let matchDetails of matchDetailsList) {
-            
-            // Common match information
 
-            const ranks = [...new Set(matchDetails.playerMatchDetails.map(details => details.League_Tier))];
-            const platforms = [...new Set(matchDetails.playerMatchDetails.map(details => details.Platform))];
-            const championBans: ChampionBan[] = [];
+            try {
 
-            matchDetails.championBans.forEach(ban => {
-                championBans.push({
-                    championId: ban.id,
-                    championName: ban.name,
-                    matchId: matchDetails.matchId,
-                    matchTimestamp: matchDetails.matchTimestamp,
-                    region: matchDetails.region,
-                    map: matchDetails.map,
-                    ranks: ranks,
-                    platforms: platforms
-                });
-            })
+                // Common match information
 
-            // Information from each player
+                const ranks = [...new Set(matchDetails.playerMatchDetails.map(details => details.League_Tier))];
+                const platforms = [...new Set(matchDetails.playerMatchDetails.map(details => details.Platform))];
+                const championBans: ChampionBan[] = [];
 
-            const championMatches: ChampionMatch[] = [];
+                matchDetails.championBans.forEach(ban => {
+                    championBans.push({
+                        championId: ban.id,
+                        championName: ban.name,
+                        matchId: matchDetails.matchId,
+                        matchTimestamp: matchDetails.matchTimestamp,
+                        region: matchDetails.region,
+                        map: matchDetails.map,
+                        ranks: ranks,
+                        platforms: platforms
+                    });
+                })
 
-            for (let playerDetails of matchDetails.playerMatchDetails) {      
-                
-                const cards: LeveledItem[] = [];
-                for (let i=1; i<=NUMBER_OF_CARDS; i++) {
-                    cards.push({
-                        id: Number(playerDetails[`ItemId${i}`]),
-                        name: String(playerDetails[`Item_Purch_${i}`]),
-                        level: Number(playerDetails[`ItemLevel${i}`])
+                // Information from each player
+
+                const championMatches: ChampionMatch[] = [];
+
+                for (let playerDetails of matchDetails.playerMatchDetails) {      
+                    
+                    const cards: LeveledItem[] = [];
+                    for (let i=1; i<=NUMBER_OF_CARDS; i++) {
+                        cards.push({
+                            id: Number(playerDetails[`ItemId${i}`]),
+                            name: String(playerDetails[`Item_Purch_${i}`]),
+                            level: Number(playerDetails[`ItemLevel${i}`])
+                        });
+                    }
+
+                    const items: LeveledItem[] = [];
+                    for (let i=1; i<=NUMBER_OF_ITEMS; i++) {
+                        items.push({
+                            id: Number(playerDetails[`ActiveId${i}`]),
+                            name: String(playerDetails[`Item_Active_${i}`]),
+                            level: Number(playerDetails[`ActiveLevel${i}`])
+                        })
+                    }
+                    
+                    championMatches.push({
+                        championId: playerDetails.ChampionId,
+                        championName: playerDetails.Reference_Name,
+                        matchId: playerDetails.Match,
+                        matchTimestamp: playerDetails.Entry_Datetime,
+                        matchResult: playerDetails.Win_Status as MatchResult,
+                        region: playerDetails.Region,
+                        map: playerDetails.Map_Game,
+                        rank: playerDetails.League_Tier,
+                        platform: playerDetails.Platform,
+                        talent: { id: playerDetails.ItemId6, name: playerDetails.Item_Purch_6 },
+                        cards: cards,
+                        items: items
                     });
                 }
 
-                const items: LeveledItem[] = [];
-                for (let i=1; i<=NUMBER_OF_ITEMS; i++) {
-                    items.push({
-                        id: Number(playerDetails[`ActiveId${i}`]),
-                        name: String(playerDetails[`Item_Active_${i}`]),
-                        level: Number(playerDetails[`ActiveLevel${i}`])
-                    })
-                }
+                // Save entries
+
+                insertedChampionMatches += await championMatchesDAL.insertMany(championMatches);
+                insertedChampionBans += await championBansDAL.insertMany(championBans);
                 
-                championMatches.push({
-                    championId: playerDetails.ChampionId,
-                    championName: playerDetails.Reference_Name,
-                    matchId: playerDetails.Match,
-                    matchTimestamp: playerDetails.Entry_Datetime,
-                    matchResult: playerDetails.Win_Status as MatchResult,
-                    region: playerDetails.Region,
-                    map: playerDetails.Map_Game,
-                    rank: playerDetails.League_Tier,
-                    platform: playerDetails.Platform,
-                    talent: { id: playerDetails.ItemId6, name: playerDetails.Item_Purch_6 },
-                    cards: cards,
-                    items: items
-                });
+            } catch(e) {
+                console.log('An error occurred while saving match details', e);
             }
-
-            // Save entries
-
-            insertedChampionMatches += await championMatchesDAL.insertMany(championMatches);
-            insertedChampionBans += await championBansDAL.insertMany(championBans);
         }
     }
 }
